4b820d87d7b586575191294579de329b1e147f1a
Andrew <andrew.shyika@gmail.com>
2025-10-13 14:41:46 +0300

    fix union

--- lab2/task1/ex_10.sql
+++ lab2/task1/ex_10.sql
@@ -3,13 +3,13 @@ FROM PC pc
 JOIN Product p ON pc.model = p.model
 WHERE p.maker = 'B'
 
-UNION
+UNION ALL
 SELECT p.maker, l.model, p.type, l.price
 FROM Laptop l
 JOIN Product p ON l.model = p.model
 WHERE p.maker = 'B'
 
-UNION
+UNION ALL
 SELECT p.maker, pr.model, p.type, pr.price
 FROM Printer pr
 JOIN Product p ON pr.model = p.model
